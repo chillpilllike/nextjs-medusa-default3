285d9585608b3654da19068cf84849d6f21e8730
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-07-27 09:42:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -25,6 +25,10 @@ const nextConfig = withStoreConfig({
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "medusa-gofinch.s3.ap-southeast-2.amazonaws.com",
+      },
     ],
   },
 })
