801be269ab435399d13aef4fd911cd2e99b34073
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-09-23 15:32:06 +0530

    Update index.tsx

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Payments Powered by Stripe.com
+            Free Shipping on orders over $50 | Hassle-Free Returns
           </Heading>
         </span>
         <a
