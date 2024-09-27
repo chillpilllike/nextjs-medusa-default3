0a31af6a48ce555ea6588a4c6b5f8c18701607af
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-09-28 00:50:03 +0530

    Update index.tsx

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -2,6 +2,7 @@ import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
+  const bgImage = 'https://www.realpointdesign.co.uk/wp-content/uploads/2015/02/Silver-Blur-Background.jpg'
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
