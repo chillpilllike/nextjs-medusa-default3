25439c45cb2249b9d374a2b3f635a2a2c5c9c4ca
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-09-28 00:54:23 +0530

    Update index.tsx

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -2,7 +2,6 @@ import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
-  const bgImage = 'https://www.realpointdesign.co.uk/wp-content/uploads/2015/02/Silver-Blur-Background.jpg'
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
@@ -14,7 +13,7 @@ const Hero = () => {
             Unique Products, Exceptional Quality & Timely Delivery
           </Heading>
           <Heading
-            level="h2"
+            level="h3"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
             Free Shipping | 30 Days Return | Secure payments
