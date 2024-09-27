f781080754d1dca6a629eeb1a60c65ef8e882971
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-09-27 23:55:21 +0530

    Update index.tsx

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -107,17 +107,33 @@ export default async function Footer() {
               <span className="txt-small-plus txt-ui-fg-base">Medusa</span>
               <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
                <li>
-  <FocusModal>
-    <FocusModal.Trigger> 
-      <span className="hover:text-ui-fg-base cursor-pointer"> 
-        GitHub 
-      </span>
-    </FocusModal.Trigger>
-    <FocusModal.Content> {/* Children are passed directly here */}
-      <FocusModal.Header>Modal Title</FocusModal.Header>
-      <FocusModal.Body>This is the modal content.</FocusModal.Body>
-    </FocusModal.Content>
-  </FocusModal>
+   <FocusModal>
+      <FocusModal.Trigger asChild>
+        <Button>Edit Variant</Button>
+      </FocusModal.Trigger>
+      <FocusModal.Content>
+        <FocusModal.Header>
+          <Button>Save</Button>
+        </FocusModal.Header>
+        <FocusModal.Body className="flex flex-col items-center py-16">
+          <div className="flex w-full max-w-lg flex-col gap-y-8">
+            <div className="flex flex-col gap-y-1">
+              <Heading>Create API key</Heading>
+              <Text className="text-ui-fg-subtle">
+                Create and manage API keys. You can create multiple keys to
+                organize your applications.
+              </Text>
+            </div>
+            <div className="flex flex-col gap-y-2">
+              <Label htmlFor="key_name" className="text-ui-fg-subtle">
+                Key name
+              </Label>
+              <Input id="key_name" placeholder="my_app" />
+            </div>
+          </div>
+        </FocusModal.Body>
+      </FocusModal.Content>
+    </FocusModal>
 </li>
                 <li>
                   <a
